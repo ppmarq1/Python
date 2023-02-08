c107a197b926c2ab79281c8400ac3ba52272b61a
Pedro Marques <ppmarq1@gmail.com>
2023-02-08 11:44:08 +0000

    Update

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ valor_1 = 10
 valor_2 = 20
 
 result = sum(valor_1,valor_2)
-print(result)
+lista_valores = []
+lista_valores.append(valor_1)
+lista_valores.append(valor_2)
+lista_valores.append(result)
+
+
+for valor in lista_valores:
+    print(valor)
